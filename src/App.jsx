722f53e75235ee3051a6fddc927ee6ab3bf544e5
yogesh <yogeshsundar142@gmail.com>
2025-11-20 12:03:50 +0530

    Lazy loading setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
+import { lazy, Suspense } from 'react'
 import './App.css'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
-import ProductList from './Components/Products/List/ProductList'
+// import ProductList from './Components/Products/List/ProductList'
 import ProtectedRoute from './Components/Auth/ProtectedRoute'
 import { Provider } from 'react-redux'
 import { store } from './Redux/store/store'
 
 function App() {
 
+  const ProductList = lazy(() => import('./Components/Products/List/ProductList'))
+
+
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/products" element={<ProductList />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/products" element={<ProductList />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </Provider>
   )
